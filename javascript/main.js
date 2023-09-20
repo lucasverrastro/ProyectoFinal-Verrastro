796b78e27e50e7b8fbb7da1d41acefffe49c0ec7
Patricio Turpin <patricioturpin@gmail.com>
2023-09-20 19:44:26 -0300

    fix carrito de compras

--- javascript/main.js
+++ javascript/main.js
@@ -32,41 +32,71 @@
 
 
 // Variables para mantener el estado del carrito
-const carrito = [];
+const cart = [];
 let total = 0;
 
 // Función para agregar un producto al carrito
 function addToCart(productName, price) {
+    console.log('productName', productName)
+    console.log('price', price)
     cart.push({ name: productName, price: price });
     total += price;
+    console.log('total', total)
+    console.log(cart)
 }
 
 // Función para actualizar el contenido del carrito
-function updateCart() {
-    const cartItemsElement = document.getElementById("productos-carrito");
-    cartItemsElement.innerHTML = "";
+// function updateCart() {
+//     const cartItemsElement = document.getElementById("productos-carrito");
+//     console.log('cartItemsElement', cartItemsElement)
+//     cartItemsElement.innerHTML = "";
 
-    cart.forEach((item, index) => {
-        const listItem = document.createElement("li");
-        listItem.textContent = `${item.name} - $${item.price.toFixed(2)}`;
-        cartItemsElement.appendChild(listItem);
-    });
+//     cart.forEach((item, index) => {
+//         const listItem = document.createElement("li");
+//         listItem.textContent = `${item.name} - $${item.price.toFixed(2)}`;
+//         cartItemsElement.appendChild(listItem);
+//     });
 
-    const cartTotalElement = document.getElementById("total-carrito");
-    cartTotalElement.textContent = `$${total.toFixed(2)}`;
-}
+//     const cartTotalElement = document.getElementById("total-carrito");
+//     cartTotalElement.textContent = `$${total.toFixed(2)}`;
+// }
 
 // Event listeners para los botones "Añadir al carrito"
-const addToCartButtons = document.querySelectorAll("add-to-cart-btn");
-addToCartButtons.forEach((button, index) => {
-    button.addEventListener("click", () => {
-        const productDescription = document.querySelectorAll("list-group-item")[index].textContent;
-        const productPrice = parseFloat(productDescription.match(/\d+\.\d+/)[0]); // Extrae el precio del texto
+// const addToCartButtons = document.querySelectorAll(".add-to-cart-btn");
+// addToCartButtons.forEach((button, index) => {
+//     button.addEventListener("click", () => {
 
-        addToCart(productDescription, productPrice);
-        updateCart();
-    });
+//         console.log('click en agregar producto')
+//         const productDescription = document.querySelectorAll(".list-group-item")[index].textContent;
+//         const productPrice = parseFloat(productDescription.match(/\d+\.\d+/)[0]); // Extrae el precio del texto
+
+//         addToCart(productDescription, productPrice);
+//         updateCart();
+//     });
+// });
+
+const addToCartButtons = document.querySelectorAll(".add-to-cart-btn");
+
+addToCartButtons.forEach(button => {
+  button.addEventListener("click", (event) => {
+    const productId = event.target.getAttribute("data-product-id");
+    console.log('productIds', productId)
+    const productDescriptionElement = document.querySelector(`.list-group-item[data-product-id="${productId}"]`);
+    const productNameElement = document.querySelector(`.card-title[data-product-id="${productId}"]`);
+
+    if (productDescriptionElement && productNameElement) {
+      const productPrice = parseInt(productDescriptionElement.textContent.match(/\d+/)[0])
+      const productName = productNameElement.textContent.trim();
+      addToCart(productName, productPrice);
+      updateCart();
+    } else {
+      console.error(`Producto con ID: ${productId} no encontrado`);
+    }
+  });
 });
+
+// Add a click event listener to each button
+
 
 // Función para eliminar un producto del carrito
 function removeFromCart(index) {
@@ -75,8 +105,10 @@ function removeFromCart(index) {
 }
 
 // Función para actualizar el contenido del carrito
+
+
 function updateCart() {
-  const cartItemsElement = document.getElementById("carrito");
+  const cartItemsElement = document.getElementById("productos-carrito");
   cartItemsElement.innerHTML = "";
 
   cart.forEach((item, index) => {
@@ -94,6 +126,9 @@ function updateCart() {
       cartItemsElement.appendChild(listItem);
   });
 
-  const cartTotalElement = document.getElementById("total-carrito");
-  cartTotalElement.textContent = `$${total.toFixed(2)}`;
+  const listItemTotal = document.createElement("li");
+  listItemTotal.textContent = `Total : $${total.toFixed(2)}`;
+  cartItemsElement.appendChild(listItemTotal)
+
 }
+
