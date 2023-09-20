11acfcdc31e680bff1fb04ce5bf67c6bd1d87391
Lucas Verrastro <lucas.verrastro@gmail.com>
2023-09-20 03:07:57 -0300

    seguimos reformando

--- javascript/main.js
+++ javascript/main.js
@@ -1,67 +1,28 @@
-const carrito = [];
+//BUSCADOR CON RECOMENDACIONES
 
-function agregar(cardId) {
+const listadoItems = ["Alma Negra Blend", "D.V Catena Zapata", "Luigi Bosca Malbec", "Nicasia Malbec", "Piattelli Arlene", "Piattelli Torrontes", "Rutini Malbec", "Salentein Malbec"];
 
-    const card = document.querySelector(`[data-id="${cardId}"]`);
-    const cardNombre = card.querySelector('.card-title').textContent;
-    const cardPrecio = parseFloat(card.querySelector('.list-group-item').textContent.replace('$', ''));
-};
-const almaNegraBlend = {
-  id: 1,  
-  nombre: "Alma Negra Blend",
-  precio: 7840,
-  subtotal: 7840,
-  cantidad: 1,
-};
-const dvCatenaZapata = {
-  id: 2,  
-  nombre: "D.V Catena Zapata",
-  precio: 7250,
-  subtotal: 7250,
-  cantidad: 1,
-};
-const luigiBoscaMalbec = {
-  id: 3,  
-  nombre: "Luigi Bosca Malbec",
-  precio: 5100,
-  subtotal: 5100,
-  cantidad: 1,
-};
-const nicasiaMalbec = {
-  id: 4,  
-  nombre: "Nicasia Malbec",
-  precio: 3500,
-  subtotal: 3500,
-  cantidad: 1,
-};
+// Seleccionamos los elementos del HTML
+const inputBuscar = document.querySelector("#buscador");
+const divResultados = document.querySelector("#resultados");
 
-carrito.push(almaNegraBlend);
-carrito.push(dvCatenaZapata);
-carrito.push(luigiBoscaMalbec);
-carrito.push(nicasiaMalbec);
+// Buscador: cada vez que se actualice el input, se dispara este detector de evento
+inputBuscar.addEventListener("input", function () {
+  const resultados = listadoItems.filter(
+    (item) =>
+      inputBuscar.value &&
+      item.toLocaleLowerCase().includes(inputBuscar.value.toLocaleLowerCase())
+  );
+  actulizarHTML(resultados);
+});
 
-actualizarCarrito();
-function actualizarCarrito() {
-    const listaCarrito = document.getElementById('Carrito');
-    const totalElement = document.getElementById('total');
-
-    // Clear the current cart display
-    listaCarrito.innerHTML = '';
-
-    // Calculate the total price
-    let total = 0;
-
-    // Loop through each item in the cart and display it
-    cart.forEach(item => {
-        const listaCarrito = document.createElement('li');
-        listaCarrito.textContent = `${card.nombre} - $${card.precio}`;
-        listaCarrito.appendChild(listaCarrito);
-        total += card.precio;
-    });
-
-    // Update the total price
-    totalElement.textContent = total.toFixed(2);
+// Función para actualizar nuestro HTML: me muestra los resultados en el
+// div #resultados
+function actulizarHTML(resultados) {
+  // Vacío el div antes de mostrar los resultados por las dudas
+  divResultados.innerHTML = "";
+  // Recorro item por item de los resultados y los muestro en párrafo
+  for (const item of resultados) {
+    divResultados.innerHTML += `<p>${item}</p>`;
+  }
 }
-
-
-
